dee480d2cba544f46725d0d47f6ef78b12ad4c29
Dan Kaiser <dskaiser82@gmail.com>
2016-08-26 18:22:41 -0700

    more testing click jquery

--- js/init.js
+++ js/init.js
@@ -5,13 +5,22 @@
     $('.parallax').parallax();
 
 
-    $( "a" ).click(function() {
-      alert( "Handler for .click() called." );
+    // $( "a" ).click(function() {
+    //   // alert( "Handler for .click() called." );
+    //
+    // });
+
+    $('a[href*=#]').click(function(event){
+      console.log(this)
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+//     event.preventDefault();
+});
+
+
     });
-
-
-
-  }); // end of document ready
+  // end of document ready
 })(jQuery); // end of jQuery name space
 
 
