84964a8cf581da5dba575fe05fd62d454e38d95e
Dan Kaiser <dskaiser82@gmail.com>
2016-08-27 09:59:27 -0700

    testing scoll top jquery

--- js/init.js
+++ js/init.js
@@ -1,27 +1,20 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    // $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
 
-    // $( "a" ).click(function() {
-    //   // alert( "Handler for .click() called." );
-    //
-    // });
-
-    $('a[href*=#]').click(function(event){
-      console.log(this)
-//     $('html, body').animate({
-//         scrollTop: $( $.attr(this, 'href') ).offset().top
-//     }, 500);
-//     event.preventDefault();
-});
 
 
     });
   // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+$('a[href*=#]').click(function(event){
+  console.log(this)
+      alert($("div").scrollTop() + " px");
+});
 
 console.log("Hi Danny")
