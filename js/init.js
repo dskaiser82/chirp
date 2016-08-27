c82c6777956c74d209da49ac2803d5ff99555c4b
Dan Kaiser <dskaiser82@gmail.com>
2016-08-27 11:13:55 -0700

    testing move works Boooooom

--- js/init.js
+++ js/init.js
@@ -4,17 +4,26 @@
     // $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-
-
-
     });
   // end of document ready
 })(jQuery); // end of jQuery name space
 
 
-$('a[href*=#]').click(function(event){
-  console.log(this)
-      alert($("div").scrollTop() + " px");
+$('#myButton').click(function() {
+   $.scrollTo($('#myDiv'), 1000);
 });
+
+
+// $('a[href*=#]').click(function(event){
+//   console.log(this)
+//
+//     event.preventDefault();
+//
+//     $("body, html").animate({
+//       scrollTop: $( $(this).attr('div') ).offset.top
+//     }, 600);
+//
+//
+// });
 
 console.log("Hi Danny")
