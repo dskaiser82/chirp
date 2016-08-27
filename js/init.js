3c950aad4474155ce2396ade83a15bcad67d4b6c
Dan Kaiser <dskaiser82@gmail.com>
2016-08-26 18:07:01 -0700

    testing click jquery

--- js/init.js
+++ js/init.js
@@ -4,7 +4,15 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+
+    $( "a" ).click(function() {
+      alert( "Handler for .click() called." );
+    });
+
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
 
 console.log("Hi Danny")
