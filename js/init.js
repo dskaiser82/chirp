4c7ed6588229a96bae24937464704d5dc4a41c10
Dan Kaiser <dskaiser82@gmail.com>
2016-08-28 17:07:07 -0700

    added subscribe href

--- js/init.js
+++ js/init.js
@@ -9,19 +9,21 @@
   // end of document ready
 })(jQuery); // end of jQuery name space
 
+//I think you dont need any javascript to go HREF to ID
 
-      //myButton is the id
-    $('#myContact').click(function() {
-      //myDiv is the href
-       $.scrollTo($('#myDiv'), 1000);
-    });
-
-      //myButton is the id
-    $('#myButton').click(function() {
-      //myDiv is the href
-       $.scrollTo($('#myDiv'), 1000);
-    });
-// $('a[href*=#]').click(function(event){
+    //
+    //   //myButton is the id
+    // $('#'+this).click(function() {
+    //   //myDiv is the href
+    //    $.scrollTo($('#myDiv'), 1000);
+    // });
+//
+//       //myButton is the id
+//     $('#myButton').click(function() {
+//       //myDiv is the href
+//        $.scrollTo($('#myDiv'), 1000);
+//     });
+// // $('a[href*=#]').click(function(event){
 //   console.log(this)
 //
 //     event.preventDefault();
