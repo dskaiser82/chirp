7cf378ef28d2350b2bb68fb37599ce01e14d7ce0
Dan Kaiser <dskaiser82@gmail.com>
2016-09-01 18:42:08 -0700

    added fa cdn

--- js/init.js
+++ js/init.js
@@ -11,28 +11,6 @@
 
 //I think you dont need any javascript to go HREF to ID
 
-    //
-    //   //myButton is the id
-    // $('#'+this).click(function() {
-    //   //myDiv is the href
-    //    $.scrollTo($('#myDiv'), 1000);
-    // });
-//
-//       //myButton is the id
-//     $('#myButton').click(function() {
-//       //myDiv is the href
-//        $.scrollTo($('#myDiv'), 1000);
-//     });
-// // $('a[href*=#]').click(function(event){
-//   console.log(this)
-//
-//     event.preventDefault();
-//
-//     $("body, html").animate({
-//       scrollTop: $( $(this).attr('div') ).offset.top
-//     }, 600);
-//
-//
-// });
+
 
 console.log("Hi Danny")
